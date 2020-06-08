5bd15fb7e2c4ac22f3d3621208fd735c99b75f10
[Maiky <[maikynata25@gmail.com]>
2020-06-08 17:38:07 -0300

    get rpservice ok

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 var req = require('request');
 var async = require('async');
 var config = require('./config');
+var configFlex = require('./configFlex');
 
 var request = req.defaults(
     {
@@ -16,7 +17,7 @@ var request = req.defaults(
 async.waterfall(
     [
         /**
-         * 1. LOGIN
+         * 1. LOGIN RoutEasy
          *
          * Routeasy currently supports plain-text credentials over HTTP.
          *
@@ -78,7 +79,7 @@ async.waterfall(
          *
          * Before run the project, you must receive data deliveries from ERP endpoint
         */
-        function(callback)
+        /* function(callback)
         {
             request(
                 {
@@ -88,8 +89,59 @@ async.waterfall(
                 }, function(err, response)
                 {
                     if(err) { console.error(err); }
-                    console.log('10. order name: ' + response.body);
+
+                    for(var key in response.body.deliveries) {
+                        if(response.body.deliveries.hasOwnProperty(key)){
+                            console.log(key + '. Customer Code: ' + response.body.deliveries[key].code);
+                            console.log(key + '. Order Number: ' + response.body.deliveries[key].order_number);
+                            console.log(key + '. NFe Number: ' + response.body.deliveries[key].invoice_number);
+                            console.log(key + '. Customer Name: ' + response.body.deliveries[key].name);
+                        }
+                      }
+                    callback();
+                });
+        }, */
+
+
+        /**
+         * 1. LOGIN FlexDB
+         *
+         * FlexDB RP Service.
+         *
+         */
+        function(callback)
+        {
+            request(
+                {
+                    json: true,
+                    url: configFlex.host + '/v1.0/auth',
+                    method: 'POST',
+                    body: configFlex.credentials
+                }, function(err, response)
+                {
+                    console.log('1. Response: ' + response.body.response.message);
                     callback();
                 });
         },
+
+        function(callback)
+        {
+            request(
+                {
+                    url: configFlex.host + '/v1.3/clientes/lastid/167216',
+                    method: 'GET'
+                }, function(err, response)
+                {
+                    if(err) { console.error(err); }
+
+                    for(var key in response.body.response.clientes) {
+                        if(response.body.response.clientes.hasOwnProperty(key)){
+                            console.log(key + '. Código Cliente: ' + response.body.response.clientes[key].codigo);
+                            console.log(key + '. Nome Cliente: ' + response.body.response.clientes[key].nome);
+                        }
+                    }
+                    callback();
+                });
+        },
+
     ]);
